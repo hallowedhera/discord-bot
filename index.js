6a03a4aeb2ee3dbeeae3a86adf70be8b8a3e12f5
hallowedhera <hallowedhera@gmail.com>
2026-05-05 19:05:05 -0400

    Push code changes

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+
+const http = require("http");
+
+http.createServer((req, res) => {
+  res.write("Bot is alive");
+  res.end();
+}).listen(process.env.PORT || 3000);
+
 const {
   Client,
   GatewayIntentBits,
