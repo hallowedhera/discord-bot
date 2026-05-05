832c0a1b7c037025ca10e0c3d6ac3200e1eacdd8
hallowedhera <hallowedhera@gmail.com>
2026-05-05 19:10:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-
 const http = require("http");
 
 http.createServer((req, res) => {
   res.write("Bot is alive");
   res.end();
-}).listen(process.env.PORT || 3000);
+}).listen(process.env.PORT);
 
 const {
   Client,
